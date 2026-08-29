a56c8bd14b7a9698f7ab0c31ea7a55023d5c6865
Eyepatch <63046602+RyanRei@users.noreply.github.com>
2026-08-29 14:00:07 +0530

    Added some tutorials

--- Scripts/Managers/TutorialManager.gd
+++ Scripts/Managers/TutorialManager.gd
@@ -2,9 +2,13 @@ class_name TutorialManager
 extends Node
 
 @export var dialog_tutorial: Control
+@export var camera:DeskCameraController
 #@export var introduction_2d: Control
 #@export var introduction_3d: Control
-
+@export var phone_light:SpotLight3D
+@export var logbook_light:SpotLight3D
+@export var pc_light:SpotLight3D
+@export var clock_light:SpotLight3D
 
 signal mouse_clicked
 func _input(event):
@@ -18,12 +22,25 @@ func welcome():
 	var label:Label=dialog_tutorial.get_node("Panel/Label")
 	print("kkkkk")
 	dialog_tutorial.show()
-	label.text="text1"
+	label.text="Who you are, where you are ahh
+"
 	await mouse_clicked
-	label.text="text2"
+	label.text="Six PM. Shift starts. You're the night operator for XXXXXX Co. 
+	Access Cards may get lost or damaged. When that happens, there's exactly one way back into their rooms, and THAT is YOUR whole job. 
+	"
+	await mouse_clicked
+	label.text="Your shift runs from 6 PM to 6 AM. Rooms close for check-out at 10 and, well, anyone calling in after that better have a very good reason
+	"
+	await mouse_clicked
+	label.text="Every decision you make tonight gets read out tomorrow morning in the daily “xxxx”. If you let the wrong person in, the resident dies. Turn away the right person, they die too except just outside instead of in. Either way, the company's numbers move up and down. Enough bad mornings, and the shelter crashes to an end.
+	So. Let's get you prepared for the first call"
+	await mouse_clicked
+	label.text="Lets take a look with help of a walkthrough"
 	await mouse_clicked
 	dialog_tutorial.hide()
 	get_tree().paused = false
+	#await introduce_logbook()
+	
 #
 #func introduce_logbook():
 	#get_tree().paused = true
